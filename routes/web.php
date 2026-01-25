559839b6b74c88313dd550b4aa5eceb2386c2051
ManpreetS0029 <manpreetmatharoo2@gmail.com>
2026-01-26 00:44:45 +0530

    Credit module added

--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Dashboard;
+use App\Livewire\Categories;
+use App\Livewire\Expenses;
+use App\Livewire\Credits;
+use App\Livewire\MonthlyTargets;
+use App\Livewire\Reports;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-use App\Livewire\Dashboard;
-
 Route::get('dashboard', Dashboard::class)
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
-
-use App\Livewire\Categories;
 
 Route::get('categories', Categories::class)
     ->middleware(['auth', 'verified'])
     ->name('categories');
 
-use App\Livewire\Expenses;
-
 Route::get('expenses', Expenses::class)
     ->middleware(['auth', 'verified'])
     ->name('expenses');
-
-use App\Livewire\Credits;
 
 Route::get('credits', Credits::class)
     ->middleware(['auth', 'verified'])
     ->name('credits');
 
-use App\Livewire\MonthlyTargets;
-
 Route::get('monthly-targets', MonthlyTargets::class)
     ->middleware(['auth', 'verified'])
     ->name('monthly-targets');
-
-use App\Livewire\Reports;
 
 Route::get('reports', Reports::class)
     ->middleware(['auth', 'verified'])
